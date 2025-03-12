d812d0ff37bc26095a0b2bc6a963adadeacc7c78
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-03-12 17:27:51 +0600

    added the dynamic buttons

--- script/index.js
+++ script/index.js
@@ -1,12 +1,25 @@
 function loadCategories() {
   //   fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    //   convert promise to json
     .then((response) => response.json())
-    .then((data) => displayCategories(data.categories));
+    // sent data to display
+    .then((data) => displayCategories(data.categories)); //returns an array of object
 }
 
 function displayCategories(categories) {
-  console.log(categories);
+  //   get the container
+  const categoryContainer = document.getElementById("category-container");
+  // loop operation in array of object
+  for (let cat of categories) {
+    // create element
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+    <button class="btn hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+    `;
+    // append the element
+    categoryContainer.append(categoryDiv);
+  }
 }
 
 loadCategories();
