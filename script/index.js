0b56fac2ddb0a07e55e1c64d993e5945bf1afd85
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-03-12 17:53:41 +0600

    added the dynamic cards

--- script/index.js
+++ script/index.js
@@ -7,6 +7,13 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories)); //returns an array of object
 }
 
+// load videos
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
+}
+
 function displayCategories(categories) {
   //   get the container
   const categoryContainer = document.getElementById("category-container");
@@ -22,4 +29,36 @@ function displayCategories(categories) {
   }
 }
 
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videos.forEach((video) => {
+    // create element
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+    <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes"
+    />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>
+      A card component has a figure, a body part, and inside body there are
+      title and actions parts
+    </p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    `;
+    // appending the element
+    videoContainer.append(videoCard);
+  });
+};
+
 loadCategories();
+loadVideos();
